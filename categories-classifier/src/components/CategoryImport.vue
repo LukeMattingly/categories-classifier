9c33b325bb5ba576202200a777d8fbd71180b75c
Kycool13 <mattingly.luke@yahoo.com>
2023-04-24 22:19:23 -0600

    added basic category view for multi line import

---
+++ categories-classifier/src/components/CategoryImport.vue
@@ -0,0 +1,24 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+        <span>Multiline message is:</span>
+        <p style="white-space: pre-line;">{{ message }}</p>
+        <textarea v-model="message" placeholder="add multiple lines"></textarea>
+    </template>
+    <template #heading>Documentation</template>
+
+    Vue’s
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+    provides you with all information you need to get started.
+  </WelcomeItem>
+</template>
+
---
+++ categories-classifier/src/views/CategoryImportView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="category">
+      <h1>This is an category page</h1>
+      <p style="white-space: pre-line;">{{ message }}</p>
+      <textarea v-model="message" placeholder="add multiple lines"></textarea>
+
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .category {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
